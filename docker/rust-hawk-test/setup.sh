83d274a65279d63a7f39b51c72c8aeded461e35e
Dustin J. Mitchell <dustin@mozilla.com>
2017-08-18 21:57:06 +0000

    Install rustup and both toolchains in the dockerfile
    The result is huge -- the toolchains are about 0.5GB each -- but this
    beats installing them anew on every task run.

---
+++ docker/rust-hawk-test/setup.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+set -ex
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+
+apt-get install -y --no-install-recommends \
+    build-essential \
+    ca-certificates \
+    curl \
+    git \
+    openssh-client \
+    libssl-dev \
+    pkg-config
+
+curl -sO https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init
+chmod +x rustup-init
+./rustup-init -y --no-modify-path
+
+# install stable
+/root/.cargo/bin/rustup install stable
+
+# install nightly + clippy
+/root/.cargo/bin/rustup install nightly
+/root/.cargo/bin/rustup run nightly cargo install clippy
+
+# install node
+curl https://nodejs.org/dist/v6.11.1/node-v6.11.1-linux-x64.tar.xz | xz -d | tar -C /usr --strip=1 -xf -
+
+# cleanup
+apt-get remove --purge -y curl
+apt-get autoremove -y
+
+rm -rf \
+    /setup.sh \
+    rustup-init \
+    /var/lib/apt/lists/* \
+    /tmp/* \
+    /var/tmp/* \
+    /root/.cargo/registry
+
+mkdir /source
